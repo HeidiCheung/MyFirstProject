abc8fa9cc50edad239337b5659f1ca20f50c87cc
HeidiCheung <31993974+HeidiCheung@users.noreply.github.com>
2017-10-15 18:25:49 +0800

    first commit
    Test

---
+++ Sealed/Sealed/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sealed
+{
+    
+        public class Information
+        {
+            public string Code { get; set; }
+            public string Name { get; set; }
+            public virtual void ShowInfo() { }
+
+        }
+
+        public sealed class JHInfo : Information
+        {
+            public sealed override void ShowInfo()
+            {
+                Console.WriteLine("進貨信息:\n" + Code + " " + Name);
+            }
+        }
+
+    public class AAInfo : JHInfo
+    {
+
+
+
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            JHInfo jhinfo = new JHInfo();
+            jhinfo.Code = "JH001";
+            jhinfo.Name = "test";
+            jhinfo.ShowInfo();
+            Console.ReadLine();
+
+        }
+    }
+}
